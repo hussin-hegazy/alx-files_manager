a33fdbf0ff8769fbc95322fb0aa4f073dffd7366
hussin-hegazy <110243753+hussin-hegazy@users.noreply.github.com>
2025-01-03 00:08:16 +0200

    Update AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,30 +1,31 @@
-// controllers/AppController.js
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
   /**
-   * Returns the status of Redis and DB
+   * should return if Redis is alive and if the DB is alive too
+   * by using the 2 utils created previously:
    * { "redis": true, "db": true } with a status code 200
    */
-  static getStatus(req, res) {
+  static getStatus(request, response) {
     const status = {
       redis: redisClient.isAlive(),
       db: dbClient.isAlive(),
     };
-    res.status(200).send(status);
+    response.status(200).send(status);
   }
 
   /**
-   * Returns the number of users and files in DB
-   * { "users": 12, "files": 1231 } with a status code 200
+   * should return the number of users and files in DB:
+   * { "users": 12, "files": 1231 }
+   *  with a status code 200
    */
-  static async getStats(req, res) {
+  static async getStats(request, response) {
     const stats = {
       users: await dbClient.nbUsers(),
       files: await dbClient.nbFiles(),
     };
-    res.status(200).send(stats);
+    response.status(200).send(stats);
   }
 }
 
