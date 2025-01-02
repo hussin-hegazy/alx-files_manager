431aa7120128d87606be99be5f8b53b6adeb961d
hussin-hegazy <110243753+hussin-hegazy@users.noreply.github.com>
2025-01-03 00:06:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-// server.js
 import express from 'express';
-import routing from './routes/index';
+import controllerRouting from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-routing(app);
+controllerRouting(app);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
