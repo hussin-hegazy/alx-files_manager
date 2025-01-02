204383dc38a0ba48711a6f4ac15445be19a56c66
hussin-hegazy <110243753+hussin-hegazy@users.noreply.github.com>
2025-01-02 23:55:53 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+// server.js
+import express from 'express';
+import routing from './routes/index';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+routing(app);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+export default app;
