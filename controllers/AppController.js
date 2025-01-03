bb6b1d9ead1b648705b39566773cfa5d2077b71d
hussin-hegazy <110243753+hussin-hegazy@users.noreply.github.com>
2025-01-03 03:10:34 +0200

    Update AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,42 +1,19 @@
-const mongoose = require('mongoose');
-const redis = require('redis');
+/* eslint-disable import/no-named-as-default */
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
-// Utility functions to check Redis and DB
-const checkRedis = () => {
-  const client = redis.createClient();
-  return new Promise((resolve, reject) => {
-    client.ping((err, result) => {
-      if (err) reject(err);
-      resolve(result === 'PONG');
+export default class AppController {
+  static getStatus(req, res) {
+    res.status(200).json({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
     });
-  });
-};
-
-const checkDB = async () => {
-  try {
-    const status = await mongoose.connection.db.admin().ping();
-    return status.ok === 1;
-  } catch (err) {
-    return false;
   }
-};
 
-module.exports.getStatus = async (req, res) => {
-  try {
-    const redisStatus = await checkRedis();
-    const dbStatus = await checkDB();
-    res.status(200).json({ redis: redisStatus, db: dbStatus });
-  } catch (error) {
-    res.status(500).json({ error: 'Unable to check services status' });
+  static getStats(req, res) {
+    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
+      .then(([usersCount, filesCount]) => {
+        res.status(200).json({ users: usersCount, files: filesCount });
+      });
   }
-};
-
-module.exports.getStats = async (req, res) => {
-  try {
-    const usersCount = await mongoose.model('User').countDocuments();
-    const filesCount = await mongoose.model('File').countDocuments();
-    res.status(200).json({ users: usersCount, files: filesCount });
-  } catch (error) {
-    res.status(500).json({ error: 'Unable to fetch stats' });
-  }
-};
+}
