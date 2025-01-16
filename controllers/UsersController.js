c5b3113edd0a15ddfdb2ef27b7649289759a1a65
hussin-hegazy <110243753+hussin-hegazy@users.noreply.github.com>
2025-01-16 03:21:34 +0200

    10, 11

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -2,7 +2,6 @@ import sha1 from 'sha1';
 import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
-import userQueue from '../utils/queue'; 
 
 class UsersController {
   static async postNew(req, res) {
